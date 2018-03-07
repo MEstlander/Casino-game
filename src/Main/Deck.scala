37c95de1c87c838c09346ac2804b63fe99c1c09d
Max Estlander <max.estlander@aalto.fi>
2018-03-07 15:44:07 +0200

    pushing

---
+++ src/Main/Deck.scala
@@ -0,0 +1,47 @@
+package Main
+import scala.math.random
+/*
+ * Creates first empty deck
+ * then makesDeck which it can shuffle aswell
+ * and can deal cards
+ */
+
+object Deck {
+  var cards = Array[Card]()
+  
+  //Makes a shuffled deck
+  def makeDeck() ={
+    var cards = Array[Card]()
+    val suits = List('H','S','D','C')
+    val values = Range(1,14).toList
+    var listOfCard = suits.flatMap(a => values.map(b => (b,a)))
+    for(i <- listOfCard){
+      if(i == (1,'H')){
+        var cards = Array[Card](new Card(1, 'H'))
+      }
+      else{
+        cards = cards :+ new Card(i._1,i._2)
+      }
+      
+    }
+    cards = cards :+ new Card(1, 'H')
+    var a = cards.toList
+    a = scala.util.Random.shuffle(a)
+    cards = a.toArray
+    cards
+  }
+  
+  def shuffleDeck():Unit = {
+    var a = this.cards.toList
+    a = scala.util.Random.shuffle(a)
+    this.cards = a.toArray
+  }
+  def dealCard(): Main.Card = {
+    // saves the first Card as a variable then 
+    // removes it from the deck
+    val Cardo = cards(0)
+    cards = cards.drop(1)
+    Cardo
+  }
+  
+}
---
+++ src/Main/Score.scala
@@ -0,0 +1,89 @@
+package Main
+
+class Score(val players: Array[Main.Player]) {
+  //counts the cards after round and gives points
+  //also announces winner if someone or multiple have >16 points
+  
+  def count(): Unit ={
+    var mostCards = Array[Main.Player](players(0))
+    for (i <- players){
+      if (mostCards(0).collected.length < i.collected.length //longest stack of collected cards
+          && i.Name != mostCards(0).Name){ // just so the first players isn't added twice
+        mostCards = Array[Main.Player](i)
+      } else if(mostCards(0).collected.length == i.collected.length ){
+        mostCards = mostCards :+ i
+      }
+    }
+    for (j <- mostCards){
+      j.score += 1
+      this.mostSpades()
+    }
+  }
+  
+  def mostSpades():Unit = {
+    var mostSpades = Array[Main.Player](players(0))
+    var spadeCount = 0
+    for (i <- players){
+      var spades = 0
+      for (j <- i.collected)
+        if(j.suit == 'S')
+          spades += 1
+      if(spades > spadeCount){
+        mostSpades = Array(i)
+      }
+      else if(spades == spadeCount){
+       mostSpades = mostSpades :+ i
+    }
+    }
+    for( k <- mostSpades){
+      k.score += 2
+    }
+    this.cardPoints
+  }
+  
+  def cardPoints(): Unit = {
+    for (i <- players){
+      for(j <- i.collected){
+        i.score += j.point
+    }
+    print("Player" + i.Name + "has" + i.score + "points\n ")
+  }
+    this.winner()
+  }
+  
+  def winner():Unit = {
+    var winners = Array[Main.Player]()
+    var evens = Array[Main.Player]()
+    for (i <- players){
+      if( i.score >= 16){
+        Game.NotOver = false
+        winners = winners :+ i
+      }
+    }
+    if (winners.length > 1){
+      var high = 0
+      for(j <- winners){
+        if(j.score > high){
+          high = j.score
+          evens = Array(j)
+          }
+        else if (j.score == high){
+          evens = evens :+ j
+        }
+        }
+      winners = evens
+      }
+    if(winners.length > 0){
+      if(winners.length > 1){
+        print ("It's a tie between:\n")
+        for (k <- winners){
+          print(k.Name + "\n")
+        }
+      }
+      else{
+        print("THE WINNER IS" + winners(0).Name + "!!!!!!")
+      }
+    }
+  }
+ 
+}
